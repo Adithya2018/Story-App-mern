2b1b12778b1e478e5a4dea325b31bcacd80b799e
Adithyan S Raj <adithyansraj20@gmail.com>
2020-12-12 20:34:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,47 @@ const path = require('path');
 const app = express()
 const PORT = process.env.PORT || 8080;
 
-mongoose.connect('', {
+const MONGODB_URI = 'mongodb+srv://Adi:JJWaGAnUDMQAEick@cluster0.k9z4b.mongodb.net/Stories?retryWrites=true&w=majority'
+
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/storyApp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () =>{
+    console.log('Mongoose is connected!!!');
+});
+
+// Schema
+const Schema = mongoose.Schema;
+const BlogPostSchema = new Schema({
+   title: String,
+   body: String,
+   date: {
+       type: String,
+       default: Date.now()
+   } 
+});
+
+// Model
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+
+//Saving data to our mongo database
+const data = {
+    title: "1. The Boy Who Cried Wolf",
+    body: "Once upon a time, there lived a shepherd boy who was bored watching his flock of sheep on the hill. To amuse himself, he shouted, “Wolf! Wolf! The sheep are being chased by the wolf!” The villagers came running to help the boy and save the sheep. They found nothing and the boy just laughed looking at their angry faces."
+};
+
+const newBlogPost = new BlogPost(data); // instance of the model
+
+// .save()
+newBlogPost.save((error) => {
+    if(error){
+        console.log("This is embarassing!");
+    }
+    else{
+        console.log("Data has been saved!!");
+    }
 });
 
 // HTTP request logger
